a458830dcfe979fda859e83c21a8f78bdf05b8bd
Finlay Thompson <finlay@dragonfly.co.nz>
2015-12-14 16:10:08 +1300

    Got it working, but still not that great

--- src/Network/GitHub.hs
+++ src/Network/GitHub.hs
@@ -11,28 +11,15 @@ module Network.GitHub
     )
 where
 
-import Control.Monad.Trans.Reader
 import Data.Proxy
-import Data.Text
-
-import Servant.API
-import Servant.Client
 
 import Network.GitHub.API
 import Network.GitHub.Types
 import Network.GitHub.Authentication
 import Network.GitHub.Client
 
-orgTeams :: OrgLogin -> GitHub [Team]
-orgTeams = ReaderT . flip call  
-  where
-    layout :: Proxy (Header "User-Agent" Text :> OAuth2Token :> OrgTeams)
-    layout = Proxy
-    call :: Maybe AuthToken -> Client OrgTeams
-    call = client layout host (Just useragent)
- 
 getOrgs :: GitHub [Organisation]
 getOrgs = github "servant-github" (Proxy :: Proxy UserOrgs)
 
---orgTeams' :: OrgLogin -> GitHub [Team]
---orgTeams' = github (Proxy :: Proxy OrgTeams)
+orgTeams :: OrgLogin -> GitHub [Team]
+orgTeams = github "servant-github" (Proxy :: Proxy OrgTeams)
--- src/Network/GitHub/Client.hs
+++ src/Network/GitHub/Client.hs
@@ -2,24 +2,15 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE InstanceSigs #-}
-{--# LANGUAGE ExistentialQuantification #-}
-{--# LANGUAGE AllowAmbiguousTypes #-}
 module Network.GitHub.Client
 where
 
 import Control.Monad.Trans.Reader
 import Control.Monad.Trans.Either
 import Data.Proxy
-import Data.Typeable
 import GHC.TypeLits
 import Data.Text
 
@@ -32,13 +23,10 @@ host :: BaseUrl
 host = BaseUrl Https "api.github.com" 443
 
 type UserAgent = Text
-useragent :: UserAgent
-useragent = "servant-github"
-
 type GitHub = ReaderT (Maybe AuthToken) (EitherT ServantError IO) 
+
 runGitHub :: GitHub a -> Maybe AuthToken -> IO (Either ServantError a)
 runGitHub comp = runEitherT . runReaderT comp 
-
 
 -- Issues that want to support in monad automatically
 -- User-Agent, Auth Token, Pagination Headers, Rate limiting 
@@ -54,95 +42,25 @@ type family GHF a :: * where
     GHF (Pre a) = GitHub a
     GHF (a -> b) = a -> GHF b
 
-
---github3 :: UserAgent -> a -> GHF a
---github3 ua (comp :: Pre a) = ReaderT (comp (Just ua))
---github3 ua (Wrap (comp :: a -> b)) = \github3 ua 
-
+-- This instance is a bit too literal. Should be possible to do it reursively
 class HasGitHub a where
-    github3 :: UserAgent -> a -> GHF a
+    embedGitHub :: UserAgent -> a -> GHF a
 instance HasGitHub (Pre a) where
-    github3 ua comp = ReaderT (comp (Just ua))
--- instance HasGitHub b => HasGitHub (a -> b) where
---     github3 ua comp arg = github3 ua (comp arg)
--- 
-github = undefined
+    embedGitHub ua comp = ReaderT (comp (Just ua))
+instance HasGitHub (a -> Pre b) where
+    embedGitHub ua comp arg = embedGitHub ua (comp arg)
+instance HasGitHub (a -> b -> Pre c) where
+    embedGitHub ua comp arg = embedGitHub ua (comp arg)
+instance HasGitHub (a -> b -> c -> Pre d) where
+    embedGitHub ua comp arg = embedGitHub ua (comp arg)
 
-github2 :: HasClient (AddHeaders api) => Proxy api -> Client (AddHeaders api)
-github2 (Proxy :: Proxy api) = client (Proxy :: Proxy (AddHeaders api)) host 
+github :: (HasClient (AddHeaders api), HasGitHub (Client (AddHeaders api))) 
+       => UserAgent -> Proxy api -> GHF (Client (AddHeaders api))
+github ua px = embedGitHub ua (clientWithHeaders px)
 
+clientWithHeaders :: HasClient (AddHeaders api) => Proxy api -> Client (AddHeaders api)
+clientWithHeaders (Proxy :: Proxy api) = client (Proxy :: Proxy (AddHeaders api)) host 
 
-class Enter typ arg ret | typ arg -> ret, typ ret -> arg where
-    enter :: arg -> typ -> ret
-
--- **  Servant combinators
-instance ( Enter typ1 arg1 ret1, Enter typ2 arg2 ret2
-         , arg1 ~ arg2
-         ) => Enter (typ1 :<|> typ2) arg1 (ret1 :<|> ret2) where
-    enter e (a :<|> b) = enter e a :<|> enter e b
-
-instance (Enter b arg ret) => Enter (a -> b) arg (a -> ret) where
-    enter arg f a = enter arg (f a)
-
--- ** Useful instances
-
--- | A natural transformation from @m@ to @n@. Used to `enter` particular
--- datatypes.
-newtype m :~> n = Nat { unNat :: forall a. m a -> n a} deriving Typeable
-
-instance Enter (m a) (m :~> n) (n a) where
-    enter (Nat f) = f
-
--- data AddHeaders a = AddHeaders 
--- 
--- instance ( HasClient (first :> last), HasClient last )
---         => HasClient (AddHeaders (first :> last)) where
---     type Client (AddHeaders (first :> last)) = Client (first :> AddHeaders last)
---     clientWithRoute (Proxy :: Proxy (AddHeaders (first :> last)))
---         = clientWithRoute (Proxy :: Proxy (first :> AddHeaders last)) 
--- 
--- instance ( HasClient layout, Client layout ~ EitherT ServantError IO a)
---          => HasClient (AddHeaders layout) where
---     type Client (AddHeaders layout) 
---         = Client (Header "User-Agent" Text :> OAuth2Token :> layout)
---     clientWithRoute (Proxy :: Proxy (AddHeaders layout))
---         = clientWithRoute (Proxy :: Proxy (Header "User-Agent" Text :> OAuth2Token :> layout))
--- 
--- type family EmbedGitHub t :: *
--- type instance EmbedGitHub (EitherT ServantError IO a) 
---     = Maybe AuthToken -> EitherT ServantError IO a
--- type instance EmbedGitHub (a -> b) = a -> EmbedGitHub b
--- 
--- --github2 :: (HasClient api, HasClient (AddHeaders api))
--- --        => Proxy api -> Client (AddHeaders api)
--- github2 (Proxy :: Proxy api) = 
---     client (Proxy :: Proxy (AddHeaders api)) host 
-
--- class HasGitHub layout where
---     type GH layout :: *
---     github :: UserAgent -> Proxy layout -> GH layout
--- 
--- instance ( HasClient (Get ct a) 
---          , Client (Get ct a) ~ EitherT ServantError IO a )
---         => HasGitHub (Get ct a) where
---     type GH (Get ct a) = GitHub a
---     github ua (Proxy :: Proxy (Get ct a)) = 
---         let layout :: Proxy (AddHeaders (Get ct a))
---             layout = Proxy
---         in  ReaderT (client layout host (Just ua))
--- 
--- instance ( KnownSymbol path, HasGitHub layout )
---         => HasGitHub (path :> layout) where
---     type GH (path :> layout) = GH layout
---     github ua (Proxy :: Proxy (path :> layout)) 
---         = github ua (Proxy :: Proxy (path :> layout))
-
--- instance ( HasGitHub layout, HasClient (arg :> layout)
---          , Client (arg :> layout) ~ (a -> Client layout) )
---         => HasGitHub (arg :> layout) where
---     type GH (arg :> layout) = a -> GH layout
---     github ua (Proxy :: Proxy (arg :> layout)) val
---         = github ua (Proxy :: Proxy (arg :> layout)) <*> return val
 
 
 
