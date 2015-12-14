73bc4fbf15f0f552d487a681dc0aab52cfa6fb97
Finlay Thompson <finlay@dragonfly.co.nz>
2015-12-15 00:18:33 +1300

    Added pagination support

--- app/Main.hs
+++ app/Main.hs
@@ -24,7 +24,7 @@ main = do
         exitFailure
 
     -- Run GitHub computation, print errors if there are any
-    errors <- runGitHub printOrgsAndTeams token
+    errors <- runGitHub (printTeams "dragonfly-science") token
     case errors of
         Left e  -> liftIO $ print e
         Right _ -> exitSuccess
@@ -41,3 +41,13 @@ printOrgsAndTeams = do
             liftIO $ T.putStrLn $ "    " <> T.intercalate ", " [ memberLogin m | m <- members]
             repos <- teamRepositories (teamId t)
             liftIO $ T.putStrLn $ "    " <> T.intercalate ", " [ repositoryName r | r <- repos]
+
+printTeams :: OrgLogin -> GitHub ()
+printTeams ol = do
+    teams <- organisationTeams ol
+    forM_ teams $ \t -> do
+        liftIO $ T.putStrLn $ "  " <> teamName t
+        members <- teamMembers (teamId t)
+        liftIO $ T.putStrLn $ "    " <> T.intercalate ", " [ memberLogin m | m <- members]
+        repos <- teamRepositories (teamId t)
+        liftIO $ T.putStrLn $ "    " <> T.intercalate ", " [ repositoryName r | r <- repos]
--- src/Network/GitHub/Client.hs
+++ src/Network/GitHub/Client.hs
@@ -5,10 +5,13 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Network.GitHub.Client
 where
 
+import Control.Monad.Trans.Class
 import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State
 import Control.Monad.Trans.Either
 import Data.Proxy
 import GHC.TypeLits
@@ -17,41 +20,106 @@ import Data.Text
 import Servant.API
 import Servant.Client
 
+import Network.HTTP.Link.Types
+import Network.HTTP.Link.Parser (parseLinkHeaderBS)
+
 import Network.GitHub.Authentication
 
 host :: BaseUrl
 host = BaseUrl Https "api.github.com" 443
 
 type UserAgent = Text
-type GitHub = ReaderT (Maybe AuthToken) (EitherT ServantError IO) 
+type GitHub = ReaderT (Maybe AuthToken) (StateT Pagination (EitherT ServantError IO))
 
 runGitHub :: GitHub a -> Maybe AuthToken -> IO (Either ServantError a)
-runGitHub comp = runEitherT . runReaderT comp 
+runGitHub comp token = runEitherT $ evalStateT (runReaderT comp token) defPagination
 
--- Issues that want to support in monad automatically
--- User-Agent, Auth Token, Pagination Headers, Rate limiting 
+-- | Issues supported in monad automatically
+-- User-Agent, Auth Token, Pagination Headers
 
 type family AddHeaders a :: * where
-    AddHeaders ((sym :: Symbol) :> last) = (sym :: Symbol) :> AddHeaders last
-    AddHeaders (first :> last)           = first :> AddHeaders last
-    AddHeaders last                      = Header "User-Agent" Text :> OAuth2Token :> last
+    AddHeaders ((sym :: Symbol) :> last) 
+        = (sym :: Symbol) :> AddHeaders last
+    AddHeaders (first :> last)
+        = first :> AddHeaders last
+    AddHeaders last 
+        = Header "User-Agent" Text :> OAuth2Token :> ReadHeaders last
+type family ReadHeaders a :: * where
+    ReadHeaders (Get cts [res])    
+        = QueryParam "page" Int :> QueryParam "per_page" Int 
+          :> Get cts (Headers '[Header "Link" Text] [res])
+    ReadHeaders (Post cts [res])   
+        = QueryParam "page" Int :> QueryParam "per_page" Int 
+          :> Post cts (Headers '[Header "Link" Text] [res])
+    ReadHeaders (Delete cts [res]) 
+        = QueryParam "page" Int :> QueryParam "per_page" Int 
+          :> Delete cts (Headers '[Header "Link" Text] [res])
+    ReadHeaders (Put cts [res])    
+        = QueryParam "page" Int :> QueryParam "per_page" Int 
+          :> Put cts (Headers '[Header "Link" Text] [res])
+    ReadHeaders (Patch cts [res])  
+        = QueryParam "page" Int :> QueryParam "per_page" Int 
+          :> Patch cts (Headers '[Header "Link" Text] [res])
+    ReadHeaders otherwise = otherwise
 
-type Pre a = Maybe UserAgent -> Maybe AuthToken -> EitherT ServantError IO a
+type Pre a = Maybe UserAgent -> Maybe AuthToken 
+            -> EitherT ServantError IO a
+
+type Pres a = Maybe UserAgent -> Maybe AuthToken 
+            -> Maybe Int -> Maybe Int
+            -> EitherT ServantError IO (Headers '[Header "Link" Text] [a])
 
 type family GHF a :: * where
-    GHF (Pre a) = GitHub a
+    GHF (Pre a)  = GitHub a
+    GHF (Pres a) = GitHub [a]
     GHF (a -> b) = a -> GHF b
 
--- This instance is a bit too literal. Should be possible to do it reursively
 class HasGitHub a where
     embedGitHub :: UserAgent -> a -> GHF a
+instance HasGitHub (Pres a) where
+    embedGitHub ua comp = do
+        token <- ask
+        lift $ do
+            rec <- gets recurse
+            if rec 
+                then modify $ \pg -> pg { page = 1, links = Nothing }
+                else return ()
+                        
+        let accumPages acc = do
+             p  <- gets page
+             pp <- gets perPage
+             hres <- lift $ comp (Just ua) token (Just p) (Just pp)
+             case getHeaders hres of
+                 [("Link", lks)] -> modify $ \pg -> pg {links = (parseLinkHeaderBS lks)}
+                 _ -> return ()
+             let acc' = acc ++ getResponse hres
+             rec <- gets recurse
+             next <- gets hasNextLink
+             if rec && next 
+                 then do
+                     modify $ \pg -> pg {page = p + 1}
+                     accumPages acc'
+                 else return acc' 
+        lift $ accumPages []
+
 instance HasGitHub (Pre a) where
-    embedGitHub ua comp = ReaderT (comp (Just ua))
+    embedGitHub ua comp = do
+        token <- ask
+        lift $ do 
+            lift $ comp (Just ua) token
+                    
+-- This instance is a bit too literal. Should be possible to do it reursively
 instance HasGitHub (a -> Pre b) where
     embedGitHub ua comp arg = embedGitHub ua (comp arg)
 instance HasGitHub (a -> b -> Pre c) where
     embedGitHub ua comp arg = embedGitHub ua (comp arg)
 instance HasGitHub (a -> b -> c -> Pre d) where
+    embedGitHub ua comp arg = embedGitHub ua (comp arg)
+instance HasGitHub (a -> Pres b) where
+    embedGitHub ua comp arg = embedGitHub ua (comp arg)
+instance HasGitHub (a -> b -> Pres c) where
+    embedGitHub ua comp arg = embedGitHub ua (comp arg)
+instance HasGitHub (a -> b -> c -> Pres d) where
     embedGitHub ua comp arg = embedGitHub ua (comp arg)
 
 github :: (HasClient (AddHeaders api), HasGitHub (Client (AddHeaders api))) 
@@ -62,5 +130,18 @@ clientWithHeaders :: HasClient (AddHeaders api) => Proxy api -> Client (AddHeade
 clientWithHeaders (Proxy :: Proxy api) = client (Proxy :: Proxy (AddHeaders api)) host 
 
 
+-- | Pagination options that can be set, including the page number, and the per_page
+data Pagination = Pagination { perPage :: Int
+                             , page    :: Int
+                             , links   :: Maybe [Link]
+                             , recurse :: Bool 
+                             } deriving Show
+defPagination :: Pagination
+defPagination = Pagination 30 1 Nothing True
+
+hasNextLink :: Pagination -> Bool
+hasNextLink pg = maybe False hnl (links pg)
+    where hnl = Prelude.any (\ln -> (Rel, "next") `elem` linkParams ln)
+    
 
 
